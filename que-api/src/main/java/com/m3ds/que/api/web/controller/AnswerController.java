4357aa23bec05d5f40ca697f3bd6968884a23423
TangZheng <632789434@qq.com>
2023-03-21 19:55:45 +0800

    -

--- que-api/src/main/java/com/m3ds/que/api/web/controller/AnswerController.java
+++ que-api/src/main/java/com/m3ds/que/api/web/controller/AnswerController.java
@@ -9,12 +9,15 @@ import com.m3ds.que.center.entity.po.Answer;
 import com.m3ds.que.center.entity.vo.AnswerVo;
 import com.m3ds.que.center.service.IAnswerService;
 import com.m3ds.que.common.core.vo.Result;
+import com.m3ds.que.common.web.validator.ValidatorUtils;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -60,6 +63,26 @@ public class AnswerController {
     public Result save(@RequestBody @Valid AnswerForm answerForm) {
         Answer answer = answerForm.toPo(Answer.class);
         answerServiceImpl.save(answer);
+        return Result.success();
+    }
+
+    /**
+     * @param answerForms 要保存的回答结果对象们
+     * @return com.m3ds.que.common.core.vo.Result
+     * @author tangzheng
+     * @date 2023/3/10 14:55
+     * @description 批量保存回答结果
+     */
+    @ApiOperation(value = "批量保存回答结果", notes = "批量保存回答结果")
+    @PostMapping("/saveBatch")
+    @Login
+    public Result save(@RequestBody @Valid List<AnswerForm> answerForms) {
+        List<Answer> answers = new ArrayList<>();
+        for (AnswerForm answerForm : answerForms) {
+            ValidatorUtils.validateEntity(answerForm);
+            answers.add(answerForm.toPo(Answer.class));
+        }
+        answerServiceImpl.saveBatch(answers);
         return Result.success();
     }
 
